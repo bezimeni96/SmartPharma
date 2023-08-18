b335bba5a68575e7b05cf45f9f4441df30605d6c
Dusan Vostic <vosticdusan@gmail.com>
2023-08-18 07:54:14 +0200

    Created About page

---
+++ src/pages/About/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+
+      <h1>Smart Pharma</h1>
+      <p>version: 0.1</p>
+      <p>Created by: Dusan Vostic</p>
+    </div>
+  );
+};
+
+export default About;
