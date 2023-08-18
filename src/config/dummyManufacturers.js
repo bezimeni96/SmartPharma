a6fa4592ce35784f3e2491af5bd6f89d4810b135
Dusan Vostic <vosticdusan@gmail.com>
2023-08-18 07:49:19 +0200

    Created dummy data

---
+++ src/config/dummyManufacturers.js
@@ -0,0 +1,7 @@
+const manufacturers = [
+  { id: "m1", name: "Manufacturer A" },
+  { id: "m2", name: "Manufacturer B" },
+  { id: "m3", name: "Manufacturer C" },
+];
+
+export default manufacturers;
---
+++ src/config/dummyProducts.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: "p1",
+    name: "Product 1",
+    manufacturer: "m1",
+    price: 10.99,
+    expiryDate: new Date("2023-12-31"),
+  },
+  {
+    id: "p2",
+    name: "Product 2",
+    manufacturer: "m2",
+    price: 25.49,
+    expiryDate: new Date("2023-09-30"),
+  },
+  {
+    id: "p3",
+    name: "Product 3",
+    manufacturer: "m3",
+    price: 5.99,
+    expiryDate: new Date("2024-02-28"),
+  },
+  {
+    id: "p4",
+    name: "Product 4",
+    manufacturer: "m1",
+    price: 15.0,
+    expiryDate: new Date("2023-10-15"),
+  },
+  {
+    id: "p5",
+    name: "Product 5",
+    manufacturer: "m2",
+    price: 8.75,
+    expiryDate: new Date("2023-11-20"),
+  },
+  {
+    id: "p6",
+    name: "Product 6",
+    manufacturer: "m3",
+    price: 12.5,
+    expiryDate: new Date("2023-12-10"),
+  },
+  {
+    id: "p7",
+    name: "Product 7",
+    manufacturer: "m1",
+    price: 6.49,
+    expiryDate: new Date("2024-01-05"),
+  },
+  {
+    id: "p8",
+    name: "Product 8",
+    manufacturer: "m2",
+    price: 19.99,
+    expiryDate: new Date("2023-10-31"),
+  },
+  {
+    id: "p9",
+    name: "Product 9",
+    manufacturer: "m3",
+    price: 3.25,
+    expiryDate: new Date("2023-09-15"),
+  },
+  {
+    id: "p10",
+    name: "Product 10",
+    manufacturer: "m1",
+    price: 7.0,
+    expiryDate: new Date("2024-03-05"),
+  },
+];
+
+export default products;
